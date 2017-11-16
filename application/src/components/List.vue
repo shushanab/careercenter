9f583a3c708d7efb8dc08b8a0896c37158271472
Shushan <shansu91@gmail.com>
2017-11-16 12:25:04 +0400

    Designed Jobs list view with main metadata Note: Under static dir added simple json structured document for testing It will be replaced by scraped data and removed from version control

--- application/src/components/List.vue
+++ application/src/components/List.vue
@@ -1,43 +1,15 @@
 <template>
   <div class="list">
-    <h1>{{ msg }}</h1>
     <router-view class="view list" name="list"></router-view>
-    <section class="section">
-
+    <section class="section" v-for="job in jobs">
       <div class="container">
-        <h1 class="title">[Job Title]</h1>
+        <h1 class="title">{{ job.job }}</h1>
         <h2 class="subtitle">
-          [A simple job <strong>details</strong> here]
+          {{ job.company }}, {{ job.location }}, {{ job.audience }}
         </h2>
+        <a><small>read more...</small></a>
       </div>
-    </section>
-
     <hr>
-    <section class="section">
-      <div class="container">
-        <h1 class="title">[Job Title]</h1>
-        <h2 class="subtitle">
-          [A simple job <strong>details</strong> here]
-        </h2>
-      </div>
-    </section>
-    <hr>
-    <section class="section">
-      <div class="container">
-        <h1 class="title">[Job Title]</h1>
-        <h2 class="subtitle">
-          [A simple job <strong>details</strong> here]
-        </h2>
-      </div>
-    </section>
-    <hr>
-    <section class="section">
-      <div class="container">
-        <h1 class="title">[Job Title]</h1>
-        <h2 class="subtitle">
-          [A simple job <strong>details</strong> here]
-        </h2>
-      </div>
     </section>
 
   </div><!-- end .container -->
@@ -45,19 +17,26 @@
 </template>
 
 <script>
+import list from '/workspace/shushan/application/static/data.json';
+
 export default {
-  name: 'List',
-  data () {
+  data() {
     return {
-      msg: 'Jobs List'
-    }
+      jobs: [
+        list[0], list[1], list[2], list[3]
+      ]
+    };
   }
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h1, h2 {
   font-weight: normal;
+}
+.list {
+  padding-top: 5%;
 }
 </style>
