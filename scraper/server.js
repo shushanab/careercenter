04ab510ed2c30e181158517de93113de80e321ac
Shushan <shansu91@gmail.com>
2017-11-17 21:25:55 +0400

    Implemented scraper functionality using phantomJs:  - connected to specified(still hard coded url)  - get document

--- scraper/server.js
+++ scraper/server.js
@@ -1,44 +1,52 @@
 var express = require("express"),
-    fs      = require("fs"),
-    request = require("request"),
-    cheerio = require("cheerio"),
-    app     = express(),
-    PORT = 8000,
-    IP = "localhost";
+    fs = require("fs"),
+    app = express(),
+    phantom = require("phantom"),
+    IP = "localhost",
+    PORT = 8000;
 
-// var ip = "212.42.202.130";
-// var http = require("http");
-// var phantom = require("phantom");
-app.set("view engine", "ejs");
+// Exit Phantom  //
+function quit(reason, value) {
+    console.log("QUIT: " + reason);
+    phantom.exit(value);
+}
 
 // ROUTERS //
-app.get("/scrape", function(req, res){
-    // TODO: Provide an input box for URL 
-    // The URL we will scrape from
-    // var url = "https://careercenter.am/index.php?/ccdspann.php?id={'\d'}";
-    var url = "https://careercenter.am/index.php?/ccdspann.php?id=29976";
-
-    // The structure of request call
-    // The first parameter is URL
-    // The callback function takes 3 parameters, an error, response status code and the html
-
-    request(url, function(error, response, html){
-        if(error) { 
-            console.log("ERROR", error);
-        } else {
-            // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
-            var $ = cheerio.load(html);
-            var a = $("frame").attr("src");
-            console.log("frame ==", html, a);
-            // Defineing the variables of scrapping: job metadata
+app.get('/scrape', function(req, res) {
+    var phantom = require("phantom");
+    var _ph, _page;
+    phantom.create().then(function(ph) {
+        _ph = ph;
+        return _ph.createPage();
+    }).then(function(page) {
+        _page = page;
+        var url = "https://careercenter.am/index.php?/ccdspann.php?id=29976";
+        return _page.open(url);
+    }).then(function(status) {
+        var value = '';
+        if (status === 'success') {
+            value = _page.evaluate(function() {
+                return document;
+            });
+            console.log("SUCCESS", value);
         }
+        return value;
+    }).then(function(value) {
+        res.json({
+            data: value
+        });
+        _page.close();
+        _ph.exit();
+    }).catch(function(e) {
+        res.json({
+            data: 'Sorry, something goes wrong:(',
+            error: e
+        });
     });
-
 });
 
-app.listen(PORT, IP, function(){
-    console.log("Scrapping from URL = http://"+IP+":"+PORT+"/scrape");
+app.listen(PORT, IP, function() {
+    console.log("Scrapping from URL = http://" + IP + ":" + PORT + "/scrape");
 });
 
 exports = module.exports = app;
-
